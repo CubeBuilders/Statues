89769cc096532e9d0d43b0a1c25dc508f3b427bc
Siggi <7286178+Siggi88@users.noreply.github.com>
2022-04-01 12:41:43 +0000

    Use a HashSet instead of an ArrayList to keep track of statues for each player

--- nms-base/src/main/java/hk/siggi/statues/PlayerInformation.java
+++ nms-base/src/main/java/hk/siggi/statues/PlayerInformation.java
@@ -1,20 +1,21 @@
 package hk.siggi.statues;
 
-import java.util.ArrayList;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import java.util.HashSet;
+import java.util.Set;
 
 public class PlayerInformation {
 
 	public final Player player;
-	public final ArrayList<Statue> shownStatues;
-	public final ArrayList<Statue> listedStatues;
+	public final Set<Statue> shownStatues;
+	public final Set<Statue> listedStatues;
 	public World lastWorld;
 
 	public PlayerInformation(Player player) {
 		this.player = player;
 		this.lastWorld = player.getWorld();
-		shownStatues = new ArrayList<Statue>();
-		listedStatues = new ArrayList<Statue>();
+		shownStatues = new HashSet<Statue>();
+		listedStatues = new HashSet<Statue>();
 	}
 }
